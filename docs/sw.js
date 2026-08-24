4309eed4c3f38c7d2953f3606fbba545345142b1
Ori Shmuel <orishmuel@Oris-MacBook-Air.local>
2026-08-23 18:49:52 -0700

    Add True Crime & Famous Heists topic (40 cards, 80 questions per language, adult audience)

--- docs/sw.js
+++ docs/sw.js
@@ -13,7 +13,7 @@
  * so any content OR shell change bumps the version and evicts stale caches.
  */
 
-const CACHE_NAME = 'trivia-cache-v-bc0ffd06';
+const CACHE_NAME = 'trivia-cache-v-00f61d2c';
 
 const ASSETS_TO_CACHE = [
   './',
